43c13c148399373443dcca1bb54397fef1838593
henryhe1234 <henryhe1123@gmail.com>
2020-09-01 19:21:22 +0000

    using nodemon to restart server, add post method to delete short url

--- express_server.js
+++ express_server.js
@@ -38,7 +38,12 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
   
-})
+});
+app.post("/urls/:shortURL/delete",(req,res)=>{
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL",(req,res)=>{
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
